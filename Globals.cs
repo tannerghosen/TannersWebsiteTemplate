e098717f33073992403bc82f16205014286c8733
Tanner Ghosen <tannerghosen@yahoo.com>
2026-06-30 13:55:59 -0400

    Misc fixes

--- Globals.cs
+++ Globals.cs
@@ -3,7 +3,6 @@
     public struct Globals
     {
         public static string AdminPassword { get; set; } // Password generated for the admin account on first setup
-        public static string DatabaseName { get; set; } // Database Name
         public static bool DisableGoogle = true; // Disable Google OAuth
         public static string DomainName { get; set; } // Domain Name
         public static bool FirstTimeRunning { get; set; } // First Time Running check
