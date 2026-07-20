73c61bee777aa013ede8041bb20cd59f5f86268d
Tanner Ghosen <tannerghosen@yahoo.com>
2026-07-20 19:07:22 -0400

    Moved some of the variables back into the model of the Admin page

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.NetworkInformation;
+using TannersWebsiteTemplate.Models;
 
 namespace TannersWebsiteTemplate.Pages
 {
@@ -7,16 +9,38 @@ namespace TannersWebsiteTemplate.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public string[]?[]? AccountsTable { get; set; }
+
+        [BindProperty]
+        public Stats stats { get; set; }
+
         public AdminModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
         public void OnGet()
         {
+            // Accounts / Security Question Tables joined as a 2d array (row, column). each user entry is 1 row, with multiple columns
+            AccountsTable = SQL.Admin.GrabAccountsTable();
+            // row[5] being modified from being a boolean true / false to a Yes / No via a select statement
+            AccountsTable = AccountsTable.Select(row =>
+            {
+                if (Convert.ToBoolean(row[5]) == true) // if the result of column 5 (Are they Admin?) is true, set it to Yes, else No
+                {
+                    row[5] = "Yes";
+                }
+                else
+                {
+                    row[5] = "No";
+                }
+                return row;
+            }).ToArray();
             if (HttpContext.Session.GetInt32("IsAdmin") != 1 || !SQL.Admin.IsAdmin(HttpContext.Session.GetInt32("UserId")))
             {
                 Response.Redirect("/Index");
             }
+            stats = Statistics.GetStats();
         }
 
         public async Task<IActionResult> OnPostDelete(int? userid)
