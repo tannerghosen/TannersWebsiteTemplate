b03ed4fc02809c75a263dbe2a8b7e4a5b112c745
Tanner Ghosen <tannerghosen@yahoo.com>
2026-07-25 22:08:33 -0400

    Moved some of the code for the first-time running on the Index page into its model

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 
 namespace TannersWebsiteTemplate.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        [BindProperty]
+        public string Password { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +16,8 @@ namespace TannersWebsiteTemplate.Pages
 
         public void OnGet()
         {
-
+            Password = Globals.AdminPassword == "" ? "" : Globals.AdminPassword;
+            Globals.AdminPassword = "";
         }
     }
 }
