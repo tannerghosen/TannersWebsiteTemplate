fc054fd1b437f8311dcc63d6d835756e4ae691a7
Tanner Ghosen <tannerghosen@yahoo.com>
2026-07-29 19:09:59 -0400

    Updated PingController

--- Controllers/PingController.cs
+++ Controllers/PingController.cs
@@ -2,11 +2,14 @@
 
 namespace TannersWebsiteTemplate.Controllers
 {
+    // Routing is pretty simple, api/[controller] is api/Ping as "Controller" is omitted.
+    // Removing api/ would make it /Ping, blanking the route it would only work if the below HttpGet is set to something.
     [ApiController]
-    [Route("api/")]
-    public class PingController : Controller
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
     {
-        [HttpGet("Ping")]
+        // if this had a name, the route would be api/Ping/(the name)
+        [HttpGet("")]
         public IActionResult Ping()
         {
             DateTime time = DateTime.Now;
