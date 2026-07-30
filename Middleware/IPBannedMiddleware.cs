aa0c3431eba9575342744d111ee133380c4b46a8
Tanner Ghosen <tannerghosen@yahoo.com>
2026-07-30 15:27:17 -0400

    Added comments to the IPBannedMiddleware class

--- Middleware/IPBannedMiddleware.cs
+++ Middleware/IPBannedMiddleware.cs
@@ -1,5 +1,4 @@
 ﻿// https://www.c-sharpcorner.com/article/middlewares-in-asp-net-core-the-ultimate-detailed-guide/
-using TannersWebsiteTemplate.SQL;
 namespace TannersWebsiteTemplate.Middleware
 {
     public class IPBannedMiddleware
@@ -11,23 +10,29 @@ namespace TannersWebsiteTemplate.Middleware
             _next = next;
         }
 
+        // To explain that what happens is when a user connects to the website, this is ran in the middleware
+        // If the client is IP banned, instead of accomplishing the request we redirect to IPBanned unless they're already there (to prevent infinite redirect loops)
+        // If the client isn't IP banned, we let the request continue as normal
         public async Task InvokeAsync(HttpContext context)
         {
             var ip = context.Connection.RemoteIpAddress?.ToString(); // the IP of the client
             var path = context.Request.Path; // their http request's path
             
-            // If we're on IPBanned, do nothing.
+            // If we're on IPBanned, do nothing. This is here to prevent infinite redirect loops from the below if statement.
             if (path.Equals("/IPBanned", StringComparison.OrdinalIgnoreCase))
             {
                 await _next(context);
                 return;
             }
+
+            // If this IP is banned, always redirect to the IPBanned page
             if (SQL.Admin.IsUserIPBannedSimple(ip))
             {
                 context.Response.Redirect("/IPBanned");
                 return;
             }
 
+            // Otherwise continue the request as normal
             await _next(context);
         }
     }
