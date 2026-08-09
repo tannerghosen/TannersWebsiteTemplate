9284e08bbaa26d3df6067c301d15ab3e955745d6
Tanner Ghosen <tannerghosen@yahoo.com>
2026-08-08 23:56:29 -0400

    Fixed an issue with site banner not properly functioning

--- Status.cs
+++ Status.cs
@@ -9,6 +9,12 @@
         public static async Task CreateAccessPassword()
         {
             AccessPassword = PasswordHelper.GeneratePassword(); // generate a password needed to actually send new updates to the websocket server (can be get'd)
+            // Remove special characters that can cause issues
+            var chars = new string[] { "!", "@", "#", "$", "%", "^", "&", "*" };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                AccessPassword = AccessPassword.Replace(chars[i], "");
+            }
         }
       
         public static string GetAccessPassword()
