4424356253705f1b652dc2cdbedcb6b8b4db4257
Tanner Ghosen <tannerghosen@yahoo.com>
2026-08-15 16:13:20 -0400

    Fixed an uncaught missing ; in ISecurityQuestion

--- Interfaces/SQL/ISecurityQuestions.cs
+++ Interfaces/SQL/ISecurityQuestions.cs
@@ -3,7 +3,7 @@
     public interface ISecurityQuestions
     {
         public static abstract Task<(string, string)> GetSecurityQuestion(int? userid);
-        public static abstract Task<(bool, bool)> CreateSecurityQuestion(int? userid, string? question, string? answer)
+        public static abstract Task<(bool, bool)> CreateSecurityQuestion(int? userid, string? question, string? answer);
         public static abstract Task<(bool, bool)> UpdateSecurityQuestion(int? userid, string? question = null, string? answer = null);
     }
 }
