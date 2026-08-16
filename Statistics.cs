aae02f3680ad60217482269fdd0c6fa2e1147cc4
Tanner Ghosen <tannerghosen@yahoo.com>
2026-08-16 14:03:27 -0400

    Added awaits to the Logger.Writes in Statistics

--- Statistics.cs
+++ Statistics.cs
@@ -5,25 +5,25 @@ namespace TannersWebsiteTemplate
     {
         public async static void IncrementLogins()
         {
-            Logger.Write("Incrementing logins", "STATS");
+            await Logger.Write("Incrementing logins", "STATS");
             await SQL.Stats.UpdateStat("logins");
         }
 
         public async static void IncrementRegistrations()
         {
-            Logger.Write("Incrementing registrations", "STATS");
+            await Logger.Write("Incrementing registrations", "STATS");
             await SQL.Stats.UpdateStat("registrations");
         }
 
         public async static void IncrementErrors()
         {
-            Logger.Write("Incrementing error total", "STATS");
+            await Logger.Write("Incrementing error total", "STATS");
             await SQL.Stats.UpdateStat("errors");
         }
 
         public async static void ResetStats()
         {
-            Logger.Write("Resetting stats", "STATS");
+            await Logger.Write("Resetting stats", "STATS");
             await SQL.Stats.ResetStats();
         }
 
