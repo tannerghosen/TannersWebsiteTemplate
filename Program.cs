6ca5a149ff8fba230401aa198158cbdfb215c1e6
Tanner Ghosen <tannerghosen@yahoo.com>
2026-08-22 20:41:15 -0400

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -115,15 +115,14 @@ builder.Services.AddAuthentication(options =>
 
 // App
 var app = builder.Build();
+app.UseHttpsRedirection();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error?error=999");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 
 app.UseStaticFiles();
 
